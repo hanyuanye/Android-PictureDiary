3afac3f94bc371e0790e228b4fbf08ee1abdaa2a
Hanyuan <h43ye@edu.uwaterloo.ca>
2018-03-25 15:21:19 -0400

    Initial Commit
    Most of the Service Logic and Database logic is finished. Need to implement a UI for the information.

--- app/src/main/java/com/example/hanyu/mypicturejournal/ImageDatabase.java
+++ app/src/main/java/com/example/hanyu/mypicturejournal/ImageDatabase.java
@@ -5,9 +5,27 @@ package com.example.hanyu.mypicturejournal;
  */
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
-@Database(entities = {Image.class}, version = 1)
+@Database(entities = {Image.class}, version = 3)
 public abstract class ImageDatabase extends RoomDatabase {
     public abstract DaoAccessImage daoAccessImage();
+    private static final String DB_NAME = "businessDatabase.db";
+    private static volatile ImageDatabase instance;
+
+    static synchronized ImageDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = create(context);
+        }
+        return instance;
+    }
+
+    private static ImageDatabase create(final Context context) {
+        return Room.databaseBuilder(
+                context,
+                ImageDatabase.class,
+                DB_NAME).build();
+    }
 }
